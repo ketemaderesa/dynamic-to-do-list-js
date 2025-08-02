fb790feda543519da1e9abfc15bb2c568a47406e
Ketema Deresa <ketemaderesa123@gmail.com>
2025-08-02 11:40:47 +0100

    inside DOMContentLoade

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM fully loaded');
+    const taskInput = document.getElementById('task-input');
+    const addButton = document.getElementById('add-task');
+    const taskList = document.getElementById('task-list');
+
+    console.log(taskInput, addButton, taskList);
 });
