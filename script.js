fe7d592decbee78305cde2089ff3d24b8873afd0
Ketema Deresa <ketemaderesa123@gmail.com>
2025-08-02 11:42:48 +0100

    Local Storage helpers

--- script.js
+++ script.js
@@ -32,4 +32,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (save) saveTaskToStorage(taskText);
     }
+    function saveTaskToStorage(taskText) {
+    const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    tasks.push(taskText);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function removeTaskFromStorage(taskText) {
+    let tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    tasks = tasks.filter(task => task !== taskText);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 });
